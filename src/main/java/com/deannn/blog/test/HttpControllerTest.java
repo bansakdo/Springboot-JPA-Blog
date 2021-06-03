84ac97505a156273bf06d5464d7eb474729df85a
bansakdo <bansakdo@gmail.com>
2021-06-03 14:01:42 +0900

    Add HTTP Method Test

---
+++ src/main/java/com/deannn/blog/test/HttpControllerTest.java
@@ -0,0 +1,29 @@
+package com.deannn.blog.test;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HttpControllerTest {
+
+    @GetMapping("/http/get")
+    public String getTest(Member m) {
+        return "get 요청: " + m.getId() + ", " + m.getUsername();
+    }
+
+    @PostMapping("/http/post")
+    public String postTest(@RequestBody Member m) {
+        return "post 요청: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
+    }
+
+    @PutMapping("/http/put")
+    public String putTest() {
+        return "put 요청";
+    }
+
+    @DeleteMapping("/http/delete")
+    public String deleteTest() {
+        return "delete 요청";
+    }
+
+}
