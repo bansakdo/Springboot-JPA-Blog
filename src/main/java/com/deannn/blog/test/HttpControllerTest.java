7ac946a37545f453eb50b6346b46531f7f38e7c9
bansakdo <bansakdo@gmail.com>
2021-06-04 14:40:31 +0900

    Add lombok test

--- src/main/java/com/deannn/blog/test/HttpControllerTest.java
+++ src/main/java/com/deannn/blog/test/HttpControllerTest.java
@@ -6,9 +6,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HttpControllerTest {
 
+    private static final String TAG = "HttpControllerTest : ";
+
+    @GetMapping("/http/lombok")
+    public String lombokTest() {
+        Member m = Member.builder().username("deannn").password("123").email("deannn@test.com").build();
+        System.out.println(TAG + "getter : " + m.getId());
+        m.setId(5000);
+        System.out.println(TAG + "setter : " + m.getId());
+        System.out.println(TAG + "getter : " + m.getUsername());
+        m.setUsername("park");
+        System.out.println(TAG + "setter : " + m.getUsername());
+        return "lombok test finished";
+    }
+
     @GetMapping("/http/get")
     public String getTest(Member m) {
-        return "get 요청: " + m.getId() + ", " + m.getUsername();
+        return "get 요청:  " + m.getId() + ", " + m.getUsername();
     }
 
     @PostMapping("/http/post")
